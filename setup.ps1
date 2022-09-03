bba9cf4356f4cc8be07ba41d42ff16b03f216d45
Oded Ginat <odedginat@gmail.com>
2022-09-04 00:00:10 +0300

    03/09/22

--- setup.ps1
+++ setup.ps1
@@ -8,12 +8,24 @@ Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.10.0/python-3.10.0.e
 
 Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.37.3.windows.1/Git-2.37.3-64-bit.exe" -OutFile "c:/temp/git.exe"
 
-c:/temp/python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+Invoke-WebRequest -Uri "https://www.mozilla.org/en-US/firefox/download/thanks/" -OutFile "c:/temp/firefox.exe"
+
+echo "installing python"
+c:/temp/python-3.10.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
 
 pause
+
+echo "installing git"
 
 c:/temp/git.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS
 
+$env:Path += ";c:\Program Files\Git\bin"
+
 pause
 
-$env:Path += ";c:\Program Files\Git\bin"
+echo "installing firefox"
+c:/temp/firefox.exe /S
+
+pause
+
+
