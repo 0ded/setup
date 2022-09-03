e8541f05b7d0b94ec7201bfa350f198bcbc26a06
Oded Ginat <odedginat@gmail.com>
2022-09-03 23:31:32 +0300

    readme.md

---
+++ setup.ps1
@@ -0,0 +1,19 @@
+start-process PowerShell -verb runas
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+mkdir c:/temp
+
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.10.0/python-3.10.0.exe" -OutFile "c:/temp/python-3.10.0.exe"
+
+Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.37.3.windows.1/Git-2.37.3-64-bit.exe" -OutFile "c:/temp/git.exe"
+
+c:/temp/python-3.7.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+
+pause
+
+c:/temp/git.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS
+
+pause
+
+
